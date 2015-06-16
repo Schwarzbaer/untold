a93a9ed3fb675611c09cd9d77aa623475e89f172
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2015-06-17 01:17:34 +0200

    Redundant test for 'if'.

--- tests/test_conditions.py
+++ tests/test_conditions.py
@@ -173,3 +173,21 @@ def test_if_node_1():
                             'special': 'exit'}]}
     s = run_through_story(story_doc)
     assert s.get_state_var('foo')
+
+def test_if_node_2():
+    story_doc = {'start_node': 'start',
+                 'story': [{'id': 'start',
+                            'scene': {'autoact': {'case': [{'if': False,
+                                                            'cond': True,
+                                                            'set': {'var': 'foo',
+                                                                    'val': {'const': False}},
+                                                            'goto': 'end'},
+                                                           {'if': True,
+                                                            'cond': True,
+                                                            'set': {'var': 'foo',
+                                                                    'val': {'const': True}},
+                                                            'goto': 'end'}]}}},
+                           {'id': 'end',
+                            'special': 'exit'}]}
+    s = run_through_story(story_doc)
+    assert s.get_state_var('foo')
