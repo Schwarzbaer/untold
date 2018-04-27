21a30c6b035cc019e3c503fbd12b220a6a404078
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2018-04-27 07:13:55 +0200

    Test properly for exception on exiting story.

--- tests/test_stories.py
+++ tests/test_stories.py
@@ -1,6 +1,8 @@
+import pytest
+
 from untold.story import eval_condition, Story, StoryExited
 from .util import run_through_story
-from numpy.testing.utils import assert_raises
+
 
 # FIXME: These need some I/O checking, akin to a REPLL.
 
@@ -9,8 +11,9 @@ def test_minimal_story():
         {'start_node': 'start',
          'story': [{'id': 'start',
                     'special': 'exit'}]}
-    s = run_through_story(story_doc)
-    assert_raises(StoryExited)
+    with pytest.raises(StoryExited):
+        s = run_through_story(story_doc, catch_exit=False)
+
 
 def test_story_with_metadata():
     story_doc = \
@@ -19,5 +22,5 @@ def test_story_with_metadata():
          'start_node': 'start',
          'story': [{'id': 'start',
                     'special': 'exit'}]}
-    s = run_through_story(story_doc)
-    assert_raises(StoryExited)
+    with pytest.raises(StoryExited):
+        s = run_through_story(story_doc, catch_exit=False)
