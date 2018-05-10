45b4344b8aad276ae78d6d50b857d19e35611de2
Sebastian Hoffmann <TheCheapestPixels@gmail.com>
2018-05-11 00:41:58 +0200

    Modularize conditions.

--- untold/conditions.py
+++ untold/conditions.py
@@ -1,41 +1,88 @@
-# Conditions ---------------------------------------------------------
+# TODO:
+# Scripting!!!
+# Give operators saner names. That way, inequality operators can be
+#   merged as well.
+# Some further possible operators:
+#   Booleans: NOT, XOR, m (or more, or fewer) out of n
+#   Range matching for numerical values
+#   One of these variables has one of these values
+#     Implicitly included:
+#       Variable has value in this list
+#       Value is in one of these variables
+#   {cond_list: [list of conditions]}
+
 
 class InvalidCondition(Exception):
     pass
 
+
+class InvalidOperator(Exception):
+    pass
+
+
+def is_equal(cond_node, state):
+    varl = eval_condition(cond_node['varl'], state)
+    varr = eval_condition(cond_node['varr'], state)
+    return varl == varr
+
+
+def is_not_equal(cond_node, state):
+    varl = eval_condition(cond_node['varl'], state)
+    varr = eval_condition(cond_node['varr'], state)
+    return varl != varr
+
+
+def is_greater_than(cond_node, state):
+    varl = eval_condition(cond_node['varl'], state)
+    varr = eval_condition(cond_node['varr'], state)
+    return varl > varr
+
+
+def is_greater_than_or_equal(cond_node, state):
+    varl = eval_condition(cond_node['varl'], state)
+    varr = eval_condition(cond_node['varr'], state)
+    return varl >= varr
+
+
+def is_lesser_than(cond_node, state):
+    varl = eval_condition(cond_node['varl'], state)
+    varr = eval_condition(cond_node['varr'], state)
+    return varl < varr
+
+
+def is_lesser_than_or_equal(cond_node, state):
+    varl = eval_condition(cond_node['varl'], state)
+    varr = eval_condition(cond_node['varr'], state)
+    return varl <= varr
+
+
+operators = {
+    '==': is_equal,
+    '!=': is_not_equal,
+    '>': is_greater_than,
+    '>=': is_greater_than_or_equal,
+    '<': is_lesser_than,
+    '<=': is_lesser_than_or_equal,
+}
+
+
 def eval_condition(cond_node, state):
-    # Returns True or False
-    # True, False: Return just that
-    # {var: varname, val: value} # DONE: Exact match: TODO: value in list-in-state-variable
-    # {var: varname, val: [value]} # TODO: Both list-in-state-variable and conditional-value can be lists
-    # {var: varname, val_gt: value1, val_lt: value2} # TODO
-    # {cond_list: [list of conditions]} # TODO
+    # Return literals as such
     if type(cond_node) in [bool, int, float, str]:
         return cond_node
-    # FIXME: This should be folded into the one above, but NoneType
-    # is undefined despite type(None) == NoneType. WTF is going on?
-    elif cond_node == None:
+    elif cond_node is None:
         return None
+    # Constants and variables
     elif 'const' in cond_node.keys():
         return cond_node['const']
     elif 'var' in cond_node.keys():
         return state.get(cond_node['var'], None)
+    # Binary operators
     elif 'op' in cond_node.keys():
         op = cond_node['op']
-        varl = eval_condition(cond_node['varl'], state)
-        varr = eval_condition(cond_node['varr'], state)
-        if op == '==':
-            return varl == varr
-        elif op == '!=':
-            return varl != varr
-        elif op == '<=':
-            return varl <= varr
-        elif op == '>=':
-            return varl >= varr
-        elif op == '<':
-            return varl < varr
-        elif op == '>':
-            return varl > varr
+        if op in operators:
+            return operators[op](cond_node, state)
+        else:
+            raise InvalidOperator
     else:
-        # This is temporary, until the new syntax is implemented.
         raise InvalidCondition
